7f6e23d35a1fbcbc22e5c94e15cf67c11ae7d083
Aymeric Dominique <aymeric.dominique@gmail.com>
2019-09-23 22:54:01 +0200

    add some defensive code

--- seloger/seloger.service.js
+++ seloger/seloger.service.js
@@ -24,24 +24,24 @@ function _digForNeighborhood(description) {
 
 
 function digForRoomCount(ad) {
-    const roomFromDetail = ad.details.detail.find(detail => detail.libelle === "Pièces")
+    const roomFromDetail = ad.details && ad.details.detail.find(detail => detail.libelle === "Pièces")
     const roomFromTitle = ad.titre && ad.titre.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "").match('([0-9]*|un|deux|trois|quatre|cinq|six|sept)? ?((piece))')
     return ad.nbPieces || (roomFromDetail && roomFromDetail.valeur) || (roomFromTitle || (isNaN(roomFromTitle[1]) ? numberString(roomFromTitle[1]) : roomFromTitle[1]))
 }
 
 function digForYearBuilt(ad) {
-    const yearFromDetail = ad.details.detail.find(detail => detail.libelle === "Année de construction")
+    const yearFromDetail = ad.details && ad.details.detail.find(detail => detail.libelle === "Année de construction")
     return yearFromDetail && yearFromDetail.valeur
 }
 
 function digForHasFurniture(ad) {
-    const furnitureFromDetail = ad.details.detail.find(detail => detail.libelle === "Meublé")
+    const furnitureFromDetail = ad.details && ad.details.detail.find(detail => detail.libelle === "Meublé")
     const furnitureFromDescription = ad.descriptif.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "").match('(?<!(non-|non ))\bmeuble')
     return !!furnitureFromDetail || (furnitureFromDescription && furnitureFromDescription.length > 1) || null
 }
 
 function digForSurface(ad) {
-    const surfaceFromDetail = ad.details.detail.find(detail => detail.libelle === "Surface")
+    const surfaceFromDetail = ad.details && ad.details.detail.find(detail => detail.libelle === "Surface")
     return ad.surfaceUnite === 'm²' ? (ad.surface || surfaceFromDetail.valeur.match(/\d+/g)[0]) : null
 }
 
