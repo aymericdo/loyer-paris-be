b4db25dd6565081b87024da1617df64069331d15
Aymeric Dominique <aymeric.dominique@gmail.com>
2019-10-02 22:30:48 +0200

    use tor to reuse leboncoin api

--- leboncoin/leboncoin.controller.js
+++ leboncoin/leboncoin.controller.js
@@ -9,26 +9,11 @@ const tr = require('tor-request')
 
 tr.TorControlPort.password = process.env.TOR_PASSWORD
 
-//print current ip
-tr.request('https://api.ipify.org', function (err, res, body) {
-    if (!err && res.statusCode == 200) {
-        console.log("Your public (through Tor) IP is: " + body);
-    }
-});
+let requestsCount = 0
 
 // routes
 router.get('/', getById)
 router.post('/data', getByData)
-router.get('/new', () => {
-    //renew to new ip
-    console.log('new ?')
-    tr.renewTorSession(function (err, msg) {
-        console.log(err)
-        if (msg) {
-            printTOR_IP();
-        }
-    });
-})
 
 function getByData(req, res, next) {
     log('getByData')
@@ -45,27 +30,46 @@ function getByData(req, res, next) {
 
 function getById(req, res, next) {
     log('getById')
-    request({
-        url: `https://api.leboncoin.fr/finder/classified/${req.query.id}`,
-    }, (error, response, body) => {
-        log('leboncoin fetched')
-        const ad = JSON.parse(body)
-        if (ad.body) {
-            digData(ad, (data) => {
-                if (data) {
-                    res.json(data)
+    let renewTorSessionPromise = Promise.resolve({})
+
+    if (requestsCount > 10) {
+        requestsCount = 0
+        renewTorSessionPromise = new Promise((resolve) => {
+            tr.renewTorSession((err, msg) => {
+                if (msg) {
+                    resolve(msg)
                 } else {
-                    res.status(409).json({
-                        error: 'no address found',
-                    })
+                    console.log(err)
                 }
             })
-        } else {
-            res.status(409).json({
-                error: 'no body found',
-            })
-        }
+        })
+    }
+
+    renewTorSessionPromise.then(() => {
+        request({
+            url: `https://api.leboncoin.fr/finder/classified/${req.query.id}`,
+        }, (error, response, body) => {
+            log('leboncoin fetched')
+            const ad = JSON.parse(body)
+            if (ad.body) {
+                digData(ad, (data) => {
+                    if (data) {
+                        res.json(data)
+                    } else {
+                        res.status(409).json({
+                            error: 'no address found',
+                        })
+                    }
+                })
+            } else {
+                res.status(409).json({
+                    error: 'no body found',
+                })
+            }
+        })
     })
+
+    requestsCount += 1
 }
 
 function getDistrict(coordinates, address) {
