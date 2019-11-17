d406539adf66937c13836f5f08f92fecf5d44973
Thomas Legrand <legrand.thomas5@hotmail.fr>
2019-11-17 17:46:05 +0100

    fix bug en prod parce que je sais pas faire du JS, my bad

--- service/address.service.js
+++ service/address.service.js
@@ -13,7 +13,7 @@ function getCoordinate(address, addressInfo) {
     const postalCode = addressInfo && addressInfo.postalCode
     const city = addressInfo && addressInfo.city
     if (city && cleanup.string(city) === 'paris') {
-        const result = getAddressInParis(address, { postalCode })
+        const result = getAddressInParis(address, { postalCode }).map(address => address.item)
         return Promise.resolve(result && { lat: result[0].fields.geom_x_y[0], lng: result[0].fields.geom_x_y[1] })
     } else {
         return opencage.geocode({ q: `${address} ${postalCode ? postalCode : ''} ${city ? city : ''}`, countrycode: 'fr' })
