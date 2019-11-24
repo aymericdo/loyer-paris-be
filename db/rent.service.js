5b81afdda4764027dcce471d7925008d0404c916
Thomas Legrand <legrand.thomas5@hotmail.fr>
2019-11-24 15:54:37 +0100

    ajoute caching de 15 min

--- db/rent.service.js
+++ db/rent.service.js
@@ -1,20 +1,29 @@
 const db = require('db')
 const Rent = db.Rent
 const log = require('helper/log.helper')
+const NodeCache = require("node-cache");
+const myCache = new NodeCache({ checkperiod: 60 * 15, deleteOnExpire: true });
 
 function getAll(onSuccess, onError) {
-    log.info('Load Rent DB')
-    Rent.find({}, function (err, rents) {
-        if (err) {
-            onError(err)
-        }
-        var rentArray = [];
+    var data = myCache.get("data");
+    if (data != undefined) {
+        log.info('Load cache of Rent DB')
+        onSuccess(data)
+    } else {
+        log.info('Load Rent DB')
+        Rent.find({}, function (err, rents) {
+            if (err) {
+                onError(err)
+            }
+            var rentArray = [];
 
-        rents.forEach(function (rent) {
-            rentArray.push(rent);
+            rents.forEach(function (rent) {
+                rentArray.push(rent);
+            });
+            success = myCache.set("data", rentArray);
+            onSuccess(rentArray)
         });
-        onSuccess(rentArray)
-    });
+    }
 }
 
 module.exports = {
