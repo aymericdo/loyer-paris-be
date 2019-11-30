d8890e66df8dd506808b6f28f674b54c5f49538f
Aymeric Dominique <aymeric.dominique@gmail.com>
2019-12-01 00:44:06 +0100

    add digdata function

--- facebook/facebook.controller.js
+++ facebook/facebook.controller.js
@@ -15,3 +15,86 @@ function getByData(req, res, next) {
             res.status(err.status).json(err)
         })
 }
+
+function digData(ad, onSuccess, onError) {
+    const yearBuilt = digService.digForYearBuilt(ad)
+    const roomCount = digService.digForRoomCount(ad)
+    const hasFurniture = digService.digForHasFurniture(ad)
+    const surface = digService.digForSurface(ad)
+    const price = digService.digForPrice(ad)
+    const [address, postalCode, city] = digService.digForAddress(ad)
+    const renter = digService.digForRenter(ad)
+    const stations = digService.digForStations(ad)
+    const charges = digService.digForCharges(ad)
+    const hasCharges = digService.digForHasCharges(ad)
+
+    if (price && surface) {
+        if (address || postalCode) {
+            if (city && !!city.length && city.toLowerCase() !== 'paris') {
+                log.error('not in Paris')
+                onError({ status: 400, msg: 'not in Paris bro', error: 'paris' })
+            } else {
+                rentFilter({
+                    address,
+                    city,
+                    hasFurniture,
+                    postalCode,
+                    roomCount,
+                    stations,
+                    yearBuilt,
+                }).then(({ match, coord }) => {
+                    if (match) {
+                        const maxAuthorized = roundNumber(+match.fields.max * surface)
+                        const priceAfterCharges = chargesService.subCharges(price, charges, hasCharges)
+                        const isLegal = price <= maxAuthorized
+
+                        saverService.rent({
+                            id: ad.id,
+                            address,
+                            city,
+                            hasFurniture,
+                            isLegal,
+                            latitude: coord && coord.lat,
+                            longitude: coord && coord.lng,
+                            maxPrice: maxAuthorized,
+                            postalCode,
+                            price,
+                            renter,
+                            roomCount,
+                            stations,
+                            surface,
+                            website: 'facebook',
+                            yearBuilt,
+                        })
+
+                        onSuccess(serializer({
+                            address,
+                            charges,
+                            hasCharges,
+                            hasFurniture,
+                            isLegal,
+                            maxAuthorized,
+                            postalCode,
+                            price,
+                            priceAfterCharges,
+                            roomCount,
+                            surface,
+                            yearBuilt,
+                        }, match))
+                    } else {
+                        log.error('no match found')
+                        onError({ status: 403, msg: 'no match found', error: 'address' })
+                    }
+                })
+            }
+        } else {
+            log.error('no address found')
+            onError({ status: 403, msg: 'no address found', error: 'address' })
+        }
+    } else {
+        log.error('minimal information not found')
+        onError({ status: 403, msg: 'minimal information not found', error: 'minimal' })
+    }
+}
+
+module.exports = router
