0df4cb294615f90bf6bb83f867f29162204d6314
Aymeric Dominique <aymeric.dominique@gmail.com>
2020-02-13 22:44:29 +0100

    update version

--- src/version.controller.ts
+++ src/version.controller.ts
@@ -3,7 +3,7 @@ const router = express.Router()
 
 router.get('/', getIsExtensionUpToDate)
 function getIsExtensionUpToDate(req: Request, res: Response, next: NextFunction) {
-    const currentVersion = '5.0.0'
+    const currentVersion = '5.0.1'
     const version = req.query.version
     res.json(currentVersion > version)
 }
