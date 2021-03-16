b8490cee75f0e9e343438c61f0e579a85e664ad3
Thomas Legrand <legrand.thomas5@hotmail.fr>
2021-03-16 11:22:59 +0100

    remove console log check

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,18 +1,11 @@
 STASH_NAME="pre-commit-$(date +%s)"
 git stash save -q --keep-index $STASH_NAME
 
-# Look for console.log
-FILES_PATTERN='\.(js|ts)(\..+)?$'
-FORBIDDEN='console.log'
-git diff --cached --name-only | \
-    grep -E $FILES_PATTERN | \
-    GREP_COLOR='4;5;37;41' xargs grep --color --with-filename -n $FORBIDDEN && echo 'COMMIT REJECTED Found "$FORBIDDEN" references. Please remove them before commiting' && exit 1
+# Test commit
+npm run test
 
 # Format code
 prettier --write "**/*.ts"
-
-# Test commit
-npm run test
 
 STASHES=$(git stash list)
 if [[ $STASHES == "$STASH_NAME" ]]; then
