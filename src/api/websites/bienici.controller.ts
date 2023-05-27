907839c4ddca7cf5855142a45c554f3de531e31d
aymeric dominique <aymeric.dominique@gmail.com>
2023-05-27 12:20:12 +0200

    remove typo

--- src/api/websites/bienici.controller.ts
+++ src/api/websites/bienici.controller.ts
@@ -1,7 +1,6 @@
 import { PrettyLog } from '@services/helpers/pretty-log'
 import { BienIci } from '@services/websites/bienici/bienici'
 import express, { Request, Response } from 'express'
-import * as fs from 'fs'
 const router = express.Router()
 
 router.post('/data/v2', getByDataV2)
@@ -9,11 +8,6 @@ function getByDataV2(req: Request, res: Response) {
   PrettyLog.call(`-> v2${req.baseUrl}/${req.body.id} getByData (${req.body.platform})`, 'blue')
   const bienici = new BienIci(res, { body: req.body })
 
-  fs.writeFile('json-data/bite.json', req.body.data, (err) => {
-    if (err) {
-      console.error(err)
-    }
-  })
   bienici.analyse()
 }
 
