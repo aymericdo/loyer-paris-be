ec9f2db18773bf4064d19ad7fc2f1a3af1ac3d93
Aymeric Dominique <aymeric.dominique@gmail.com>
2024-04-23 12:38:52 +0200

    fix warning

--- src/db/zone.model.ts
+++ src/db/zone.model.ts
@@ -2,7 +2,6 @@ import mongoose from 'mongoose'
 const Schema = mongoose.Schema
 
 const schema = new Schema({
-  _id: { type: Number, required: true, unique: true },
   type: { type: String, required: true },
   geometry: { type: Object, required: true },
   properties: { type: Object, required: true },
