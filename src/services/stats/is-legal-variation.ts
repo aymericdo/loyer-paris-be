8aad3170745f30988106093bfee98a4b2635f93c
Aymeric Dominique <aymeric.dominique@gmail.com>
2024-07-13 22:10:23 +0200

    singlequote

--- src/services/stats/is-legal-variation.ts
+++ src/services/stats/is-legal-variation.ts
@@ -44,32 +44,32 @@ export function getIsLegalVariation(req: Request, res: Response) {
         data: {
           values: data,
         },
-        "encoding": {
-          "x": {
-            "field": "weekDate",
-            "type": "ordinal",
-            "title": "Semaine"
+        encoding: {
+          x: {
+            field: 'weekDate',
+            type: 'ordinal',
+            title: 'Semaine'
           },
-          "y": {
-            "field": "illegalPercentageDecimal",
-            "type": "quantitative",
-            "title": "Pourcentage illégal (%)",
-            "axis": { "format": ".1%" }
+          y: {
+            field: 'illegalPercentageDecimal',
+            type: 'quantitative',
+            title: 'Pourcentage illégal (%)',
+            axis: { format: '.1%' }
           },
-          "tooltip": [
-            { "field": "weekDate", "type": "nominal", "title": "Semaine" },
-            { "field": "illegalPercentageDecimal", "type": "quantitative", "title": "Pourcentage illégal", "format": ".1%" },
-            { "field": "totalCount", "type": "quantitative", "title": "Total" }
+          tooltip: [
+            { field: 'weekDate', type: 'nominal', 'title': 'Semaine' },
+            { field: 'illegalPercentageDecimal', type: 'quantitative', title: 'Pourcentage illégal', format: '.1%' },
+            { field: 'totalCount', type: 'quantitative', 'title': 'Total' }
           ]
         },
-        "layer": [
+        layer: [
           {
-            "mark": { "type": "line", "point": true, "interpolate": "monotone" }
+            mark: { type: 'line', 'point': true, 'interpolate': 'monotone' }
           },
           {
-            "mark": { "type": "text", "align": "left", "dx": 5, "dy": -5 },
-            "encoding": {
-              "text": { "field": "illegalPercentageDecimal", "type": "quantitative", "format": ".1%" }
+            mark: { type: 'text', align: 'left', dx: 5, dy: -5 },
+            'encoding': {
+              text: { field: 'illegalPercentageDecimal', type: 'quantitative', format: '.1%' }
             }
           }
         ]
