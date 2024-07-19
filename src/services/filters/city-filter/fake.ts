33b318544e9899b033aae16980aa7054d2ac89c6
Aymeric Dominique <aymeric.dominique@gmail.com>
2024-07-19 16:01:32 +0200

    done

--- src/services/filters/city-filter/fake.ts
+++ src/services/filters/city-filter/fake.ts
@@ -3,6 +3,7 @@ import { AvailableMainCities } from '@services/filters/city-filter/city-list'
 export const isFake = (city: AvailableMainCities): boolean => {
   switch (city) {
     case 'la rochelle':
+      // nice, nantes, strasbourg, grenoble, rennes, toulon, tours, annecy
       return true
     default:
       return false
