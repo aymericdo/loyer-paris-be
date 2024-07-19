27de25f1ba7b12fefa2a222860466636b549ffd5
Aymeric Dominique <aymeric.dominique@gmail.com>
2024-07-19 17:18:08 +0200

    insee code

--- src/services/diggers/address/address-service.ts
+++ src/services/diggers/address/address-service.ts
@@ -1,6 +1,7 @@
 import { DataGouvAddress, DataGouvAddressItem } from '@interfaces/address'
 import { Coordinate } from '@interfaces/shared'
 import { AvailableCities } from '@services/filters/city-filter/city-list'
+import { codeInsee } from '@services/filters/city-filter/code-insee'
 import axios from 'axios'
 
 export abstract class AddressService {
@@ -9,7 +10,7 @@ export abstract class AddressService {
   static async getAddresses(city: AvailableCities, query: string): Promise<DataGouvAddressItem[]> {
     const limit = 5
 
-    const result = await axios.get(`https://api-adresse.data.gouv.fr/search/?q=${query}+${city}&limit=${limit}&autocomplete=1`)
+    const result = await axios.get(`https://api-adresse.data.gouv.fr/search/?q=${query}&limit=${limit}&citycode=${codeInsee(city)}&autocomplete=1`)
     const dataGouv: DataGouvAddress = result.data
     return dataGouv.features
   }
