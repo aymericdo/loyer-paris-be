b4b4075f8ca50e86d86023d21e70c11a0d71811f
Aymeric Dominique <aymeric.dominique@gmail.com>
2024-07-19 17:23:40 +0200

    error

--- src/services/diggers/address/address-service.ts
+++ src/services/diggers/address/address-service.ts
@@ -2,6 +2,7 @@ import { DataGouvAddress, DataGouvAddressItem } from '@interfaces/address'
 import { Coordinate } from '@interfaces/shared'
 import { AvailableCities } from '@services/filters/city-filter/city-list'
 import { codeInsee } from '@services/filters/city-filter/code-insee'
+import { PrettyLog } from '@services/helpers/pretty-log'
 import axios from 'axios'
 
 export abstract class AddressService {
@@ -10,8 +11,12 @@ export abstract class AddressService {
   static async getAddresses(city: AvailableCities, query: string): Promise<DataGouvAddressItem[]> {
     const limit = 5
 
-    const result = await axios.get(`https://api-adresse.data.gouv.fr/search/?q=${query}&limit=${limit}&citycode=${codeInsee(city)}&autocomplete=1`)
-    const dataGouv: DataGouvAddress = result.data
-    return dataGouv.features
+    try {
+      const result = await axios.get(`https://api-adresse.data.gouv.fr/search/?q=${query}+${city}&limit=${limit}&citycode=${codeInsee(city)}&autocomplete=1`)
+      const dataGouv: DataGouvAddress = result.data
+      return dataGouv.features
+    } catch (error) {
+      PrettyLog.call(JSON.stringify(error), 'red')
+    }
   }
 }
