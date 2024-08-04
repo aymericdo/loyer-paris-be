1f28936bdb27ab5b10b7d48e96ff1b48187a06fe
aymeric dominique <aymeric.dominique@gmail.com>
2024-08-04 12:05:18 +0200

    more precise for the city

--- src/services/websites/locservice/locservice.scrapping.ts
+++ src/services/websites/locservice/locservice.scrapping.ts
@@ -16,7 +16,7 @@ export class LocserviceScrapping {
     const price = document.querySelector('#resume_detail > ul > li.loyer')
     const furnished = document.querySelector('#resume_detail > ul > li.meuble')
     const surface = document.querySelector('#resume_detail > ul > li.surface')
-    const cityLabel = document.querySelector('#YouAreThere > div:nth-child(1)')
+    const cityLabel = document.querySelector('#YouAreThere')
 
     const dpe = document.querySelector('#resume_detail > ul > li.dpe > div:nth-child(1) > span')
 
