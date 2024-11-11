e6b1b1c95e319a65b7088e2c9fd8b2e1b597dc3d
Aymeric Dominique <aymeric.dominique@gmail.com>
2024-11-11 21:44:17 +0100

    charges

--- src/services/helpers/regex.ts
+++ src/services/helpers/regex.ts
@@ -6,9 +6,9 @@ export const regexString = (value: string): string => {
     furnished: /(?<!(non-|non ))\bmeuble/g,
     nonFurnished: /(non-|non )\bmeuble/g,
     charges:
-      /((?<=(charges locatives |charges |charge )(\D{0,4}))|(?<=\d{3,4}((|\.|,)\d{1,2})?(\D{0,5})?\+ ?))(\d{2,3}((|\.|,)\d{1,2})?)(?=( ?euros\b| ?€| ?e\b| ?eur\b| ?euro\b | ?de\b ?(charge(s)?)\b))/g,
+      /((?<=((?<! hors )charges locatives |(?<! hors )charges |(?<! hors )charge )(\D{0,4}))|(?<=\d{3,4}((|\.|,)\d{1,2})?(\D{0,5})?\+ ?))(\d{2,3}((|\.|,)\d{1,2})?)(?=( ?euros\b| ?€| ?e\b| ?eur\b| ?euro\b | ?de\b ?(charge(s)?)\b))/g,
     hasCharges: /prix charge[s]? comprise[s]?/g,
-    surface: /(?<=[^0-9])(9|\d{2}((|\.|,)\d{1,2})?)(?= ?(m2\b|metre|m²))/g,
+    surface: /((9|\d{2})((|\.|,)\d{1,2})?)(?= ?(m2\b|metre|m²))/g,
     isHouse: /maison/g,
     particulier: /particulier/g,
   }
