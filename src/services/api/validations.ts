6c9901970190bdf270da134ecf1d6bf8d417c7c2
Aymeric Dominique <aymeric.dominique@gmail.com>
2025-02-18 00:47:07 +0100

    undefined

--- src/services/api/validations.ts
+++ src/services/api/validations.ts
@@ -23,5 +23,5 @@ export function paramMiddleware(allAccepted = false) {
 }
 
 export function queryParamValidator(value: string) {
-  return (value && value !== 'null') ? value : null
+  return (value && value !== 'null' && value !== 'undefined') ? value : null
 }
