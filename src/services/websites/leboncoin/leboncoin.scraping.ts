ce7a57fde2b2e2cb8a34fb9a3d65c3e4002419b7
Aymeric Dominique <aymeric.dominique@gmail.com>
2025-05-20 15:24:18 +0200

    lbc ok

--- src/services/websites/leboncoin/leboncoin.scraping.ts
+++ src/services/websites/leboncoin/leboncoin.scraping.ts
@@ -13,9 +13,9 @@ export class LeboncoinScraping {
     const body = document.querySelector('[data-qa-id=adview_description_container]')
 
     const price = document.querySelector('[data-qa-id=adview_price]')
-    const renter = document.querySelector('#aside > section > div > [class*="profile"]')
+    const renter = document.querySelector('#aside > section > div > div > div')
     const hasCharges = document.querySelector('[data-qa-id=adview_price] > div p.text-caption')
-    const cityLabel = document.querySelector('#grid > article [href=\'#map\']')
+    const cityLabel = [...document.querySelectorAll('#mainContent > div > div > nav > div > div > nav > ol > li')].map(el => el.textContent).join(' ')
 
     const surface = document.querySelector(
       '#grid > article div div[data-qa-id=criteria_item_square] > div > div div p'
@@ -30,7 +30,7 @@ export class LeboncoinScraping {
 
     const dpe = document.querySelector('#grid > article div div[data-qa-id=criteria_item_energy_rate] .styles_active__2SvNh')
 
-    if ((!subject && !body) || !price || !cityLabel) {
+    if ((!subject && !body) || !price) {
       return null
     }
 
@@ -38,7 +38,7 @@ export class LeboncoinScraping {
       id: null,
       body: (body ? body.textContent : ''),
       hasCharges: hasCharges?.textContent === 'Charges comprises',
-      cityLabel: cityLabel?.textContent,
+      cityLabel,
       dpe: dpe?.textContent,
       furnished: furnished?.textContent,
       price: price?.textContent,
