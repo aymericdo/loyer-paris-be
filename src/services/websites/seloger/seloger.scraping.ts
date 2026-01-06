fd7ebfe4158b68b371528e93746f150af8cbe7cf
Aymeric Dominique <aymeric.dominique@gmail.com>
2026-01-06 11:47:42 +0100

    fix scrapping

--- src/services/websites/seloger/seloger.scraping.ts
+++ src/services/websites/seloger/seloger.scraping.ts
@@ -12,48 +12,41 @@ export class SelogerScraping {
 
     const title = document.querySelector('[data-testid*="cdp-hardfacts"]')
     const description = document.querySelector('.Section.Description')
-    const price = document.querySelector(
-      '[data-testid*="cdp-seo-wrapper"] .css-k0qv01',
-    )
-    const hasCharges = document.querySelector(
-      '[data-testid*="cdp-seo-wrapper"] .css-19wvzv',
-    )
+    const price = title?.nextElementSibling
+    const hasCharges = title?.nextElementSibling
     const cityLabel = document.querySelector(
       '[data-testid*="cdp-location-address"]',
     )
     const renter = document.querySelector('[data-testid*="ContactCard.Title"]')
-    const chargesElement = document.querySelector(
-      '[data-testid*="Sections.Price.PrimaryComponent"]',
-    )
+
+    const priceElement = document.querySelector('#price')
+    const chargesElement = priceElement.nextElementSibling
 
     const charges = chargesElement?.textContent?.match(
       /(?<=(pour charges.*))\d+/,
     )
 
-    const itemTags = [
-      ...document.querySelectorAll(
-        '[data-testid*="cdp-seo-wrapper"] .css-7tj8u',
-      ),
-    ]
+    const itemTags = document
+      .querySelector('[data-testid*="cdp-hardfacts-keyfacts"]')
+      .textContent?.split('•')
+      .map((text: string) => text.trim())
 
     let isParticulier = false
     let surface = null
     let rooms = null
 
-    itemTags.forEach((tag) => {
-      if (tag.textContent.match(/m²/g) && !tag.textContent.match(/terrain/gi)) {
+    itemTags.forEach((tag: string) => {
+      if (tag.match(/m²/g) && !tag.match(/terrain/gi)) {
         surface = tag
-      } else if (tag.textContent.match(/pièce/gi)) {
+      } else if (tag.match(/pièce/gi)) {
         rooms = tag
-      } else if (tag.textContent.match(/Particulier/g)) {
+      } else if (tag.match(/Particulier/g)) {
         isParticulier = true
       }
     })
 
     const itemTags2 = [
-      ...document.querySelectorAll(
-        'ul li [data-testid*="Sections.Features.Feature"]',
-      ),
+      ...document.querySelectorAll('[data-testid*="cdp-features"] ul li'),
     ]
 
     const furnished = itemTags2?.some((el) => {
@@ -80,7 +73,7 @@ export class SelogerScraping {
     const cityLabelText = cityLabel && cityLabel.textContent
 
     const dpeElem = document.querySelector(
-      '[data-testid*="Energy.Preview.EfficiencyClass"]',
+      '[data-testid*="cdp-preview-scale-highlighted"]',
     )
 
     return {
@@ -89,7 +82,7 @@ export class SelogerScraping {
       charges: charges?.length ? charges[0] : null,
       description: description?.textContent,
       furnished,
-      hasCharges: hasCharges?.textContent.includes('cc'),
+      hasCharges: hasCharges?.textContent.includes('charges comprises'),
       price: price?.textContent,
       renter: isParticulier ? PARTICULIER : renter ? renter.textContent : null,
       rooms: rooms?.textContent,
