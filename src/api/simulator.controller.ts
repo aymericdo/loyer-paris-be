63d739fef543ce6bceb3e2199edeac9dfe533671
Aymeric Dominique <aymeric.dominique@gmail.com>
2026-03-16 12:14:43 +0100

    mon loyers

--- src/api/simulator.controller.ts
+++ src/api/simulator.controller.ts
@@ -5,6 +5,6 @@ import express from 'express'
 const router = express.Router()
 
 router.get('/:city', paramMiddleware(), simulator)
-router.get('mon-loyer/:city', paramMiddleware(), simulator)
+router.get('/mon-loyer/:city', paramMiddleware(), simulator)
 
 export default router
